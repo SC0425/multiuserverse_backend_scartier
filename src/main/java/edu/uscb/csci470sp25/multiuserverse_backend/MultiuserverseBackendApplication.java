22a34f18fcf01aa45245980052a95c1de5a7c140
SC <SC@192.168.1.3>
2025-03-07 17:32:48 -0500

    initial commit

---
+++ src/main/java/edu/uscb/csci470sp25/multiuserverse_backend/MultiuserverseBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp25.multiuserverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiuserverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiuserverseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uscb/csci470sp25/multiuserverse_backend/MultiuserverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uscb.csci470sp25.multiuserverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiuserverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

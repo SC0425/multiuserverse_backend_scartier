919373d0a27e6e81d1a605e5cd19412bcbe1c2a9
SC0425 <SCARTIER@email.uscb.edu>
2025-03-17 20:24:05 -0400

    added and tested code to enable adding a new user to the database

---
+++ src/main/java/edu/uscb/csci470sp25/multiuserverse_backend/controller/UserController.java
@@ -0,0 +1,51 @@
+package edu.uscb.csci470sp25.multiuserverse_backend.controller;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort; // needed for sorting list of users by ID
+import org.springframework.web.bind.annotation.*;
+
+import edu.uscb.csci470sp25.multiuserverse_backend.exception.UserNotFoundException;
+import edu.uscb.csci470sp25.multiuserverse_backend.model.User;
+import edu.uscb.csci470sp25.multiuserverse_backend.repository.UserRepository;
+
+@RestController
+// @CrossOrigin("http://localhost:5173") // <-- uncomment this when narrator codes @CrossOrigin in video #9
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Stub: Always throws UnsupportedOperationException (Test should fail initially)
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    // Stub: Returns an empty list (Test should fail if expecting actual users)
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return Collections.emptyList();
+    }
+
+    // Stub: Always throws UserNotFoundException (Test should fail when expecting user data)
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id) {
+        throw new UserNotFoundException(id);
+    }
+
+    // Stub: Always throws UnsupportedOperationException (Test should fail when expecting update)
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        throw new UnsupportedOperationException("Method not implemented yet");
+    }
+
+    // Stub: Always throws UserNotFoundException (Test should fail when expecting deletion)
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id) {
+        throw new UserNotFoundException(id);
+    }
+
+} // end class UserController
